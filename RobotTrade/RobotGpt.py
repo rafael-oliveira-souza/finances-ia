f7fc2207530ee0031a3f74741a4f4d59df5f619a
Rafael Oliveira de Souza <rafaelsoec@gmail.com>
2026-01-19 19:20:07 -0300

    Removendo keys

--- RobotTrade/RobotGpt.py
+++ RobotTrade/RobotGpt.py
@@ -60,7 +60,7 @@ class RobotGpt:
 
         self.account = self.conectar_mt5(login, senha, servidor)
         self.client = OpenAI(
-            api_key="Teste APiKey")
+            api_key="Teste")
         self.perplexityClient = PerplexityClient()
 
     def conectar_mt5(self, login: int, senha: str, servidor: str, caminho_terminal: str = None):
